41c44448a66be90d1e9c484a51ad8b8f86663ef1
Anwar Ahmad <anwaar@modash.io>
2021-02-16 18:41:44 +0100

    reply router

--- service-controller/src/routes/reply.routes.ts
+++ service-controller/src/routes/reply.routes.ts
@@ -1,13 +1,35 @@
 import { Express, Request, Response } from 'express'
+import { createReply, deleteReply, findReply } from '../services/intent-message.service'
 
 
 export default (app: Express) => {
 
-    app.put('/reply/intent', (req: Request, res: Response) => {
-        
+    app.post('/reply/intent', async (req: Request, res: Response) => {
+        const { message, intentName } = req.body
+
+        const resp = await createReply({
+            message,
+            intentName
+        })
+
+        res.send({ success: resp })
     })
 
-    app.delete('/reply/intent', (req: Request, res: Response) => {
+    app.delete('/reply/intent', async (req: Request, res: Response) => {
+        const { intentName } = req.body
 
+        const resp = await deleteReply(intentName)
+        res.send({ success: resp })
+    })
+
+    app.get('/reply/intent', async (req: Request, res: Response) => {
+        const name: string = String(req.query.name)
+
+        const reply = await findReply(name)
+
+        res.send({
+            success: true,
+            message: reply
+        })
     })
 }
